f2a8ee7433064b6fc3307cf0f44e501d44f1e573
Valeska <missvaleskavv@gmail.com>
2017-05-29 01:57:24 -0700

    Working user login system via  firebase and google, improved navigation drawer with profile images, beginnings of the implementation of our own server-database system.

--- app/src/main/java/com/ukie/ukie/Questions.java
+++ app/src/main/java/com/ukie/ukie/Questions.java
@@ -7,15 +7,71 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
 import android.widget.CheckedTextView;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.ui.User;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import static com.ukie.ukie.R.color.md_amber_600;
+import static com.ukie.ukie.R.color.md_red_800;
 
 public class Questions extends AppCompatActivity implements View.OnKeyListener {
+
+    /*public class Conjugations {
+        public Conjugations() {}
+
+        String Infinitive;
+        String Conjugation;
+        String ConjugationType;
+
+        public String getInfinitive() {
+            return Infinitive;
+        }
+
+        public String getConjugation() {
+            return Conjugation;
+        }
+
+        public String getConjugationType() {
+            return ConjugationType;
+        }
+
+    }*/
+
+    private DatabaseReference myRef;
+    private static final String TAG = "SignInActivity";
+
+    TextView QuestionText = (TextView) findViewById(R.id.textView2);
+
+    Button checkBtn = (Button) findViewById(R.id.button2);
+
+    TextView Infin = (TextView) findViewById(R.id.Infin);
+    TextView Conj = (TextView) findViewById(R.id.Conj);
+    TextView ConjType = (TextView) findViewById(R.id.ConjType);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +85,74 @@ public class Questions extends AppCompatActivity implements View.OnKeyListener {
         final EditText textView = (EditText) findViewById(R.id.editText);
         textView.setOnKeyListener(this);
 
+        //Infin.setText(serverConnect("http://192.168.0.135:3000/infin"));
+        //Conj.setText(serverConnect("http://192.168.0.135:3000/conj"));
+        //ConjType.setText(serverConnect("http://192.168.0.135:3000/conjtype"));
+
+        checkBtn.setBackgroundColor(getResources().getColor(md_amber_600));
+
+        /*FirebaseDatabase database = FirebaseDatabase.getInstance();
+        myRef = database.getReference("conjugation");
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                Conjugations conj = dataSnapshot.getValue(Conjugations.class);
+
+                Infin.setText(conj.getInfinitive());
+                Conj.setText(conj.getConjugation());
+                ConjType.setText(conj.getConjugationType());
+                QuestionText.setText("Conjugate " + Infin.getText() + " for " + ConjType.getText());
+
+                //Log.d(TAG, "User name: " + conj.getInfinitive() + ", email " + conj.getConjugation());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });*/
+
+    }
+
+    public String serverConnect(String url) {
+        HttpClient httpclient = new DefaultHttpClient();
+        HttpGet httpget = new HttpGet(url);
+        String line = null;
+        try {
+            HttpResponse response = httpclient.execute(httpget);
+            if(response != null) {
+                InputStream inputstream = response.getEntity().getContent();
+                line = convertStreamToString(inputstream);
+                Toast.makeText(this, line, Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Unable to complete your request", Toast.LENGTH_LONG).show();
+            }
+        } catch (ClientProtocolException e) {
+            Toast.makeText(this, "Caught ClientProtocolException", Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            Toast.makeText(this, "Caught IOException", Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            Toast.makeText(this, "Caught Exception", Toast.LENGTH_SHORT).show();
+        }
+
+        return line;
+    }
+
+    public String convertStreamToString(InputStream is) {
+        String line = null;
+        StringBuilder total = new StringBuilder();
+        BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+        try {
+            while ((line = rd.readLine()) != null) {
+                total.append(line);
+            }
+        } catch (Exception e) {
+            Toast.makeText(this, "Stream Exception", Toast.LENGTH_SHORT).show();
+        }
+        return total.toString();
     }
 
     @Override
@@ -44,7 +168,7 @@ public class Questions extends AppCompatActivity implements View.OnKeyListener {
 
             String tmp = ((EditText) v).getText().toString();
 
-            if (tmp.compareToIgnoreCase("живеш") == 0) {
+            if (tmp.compareToIgnoreCase(String.valueOf(Conj.getText())) == 0) {
                 textView1.setText("Correct!");
                 textView1.setChecked(true);
                 //textView1.setTextColor(100);
@@ -52,8 +176,9 @@ public class Questions extends AppCompatActivity implements View.OnKeyListener {
                     prog.setProgress(prog.getProgress() + 1);
                 }
             } else {
-                textView1.setText("That's incorrect, the correct conjugation for ти of жити is \"живеш\"");
+                textView1.setText("That's incorrect, the correct conjugation for " + ConjType.getText() + " of " + Infin.getText() + " is \"" + Conj.getText() + "\"");
                 textView1.setChecked(false);
+                checkBtn.setBackgroundColor(getResources().getColor(md_red_800));
                 //textView1.setTextColor(500);
                 if (prog.getProgress() != 0) {
                     prog.setProgress(prog.getProgress() - 1);
--- app/src/main/java/com/ukie/ukie/google_login.java
+++ app/src/main/java/com/ukie/ukie/google_login.java
@@ -2,15 +2,24 @@ package com.ukie.ukie;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.MenuPopupWindow;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -21,6 +30,32 @@ import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.common.api.Status;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.mikepenz.google_material_typeface_library.GoogleMaterial;
+import com.mikepenz.iconics.IconicsDrawable;
+import com.mikepenz.itemanimators.AlphaCrossFadeAnimator;
+import com.mikepenz.materialdrawer.AccountHeader;
+import com.mikepenz.materialdrawer.AccountHeaderBuilder;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.ExpandableDrawerItem;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
+import com.mikepenz.materialdrawer.model.ProfileSettingDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
+import com.mikepenz.materialdrawer.model.SectionDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IProfile;
+import com.mikepenz.materialdrawer.util.AbstractDrawerImageLoader;
+import com.mikepenz.materialdrawer.util.DrawerImageLoader;
+import com.squareup.picasso.Picasso;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
@@ -32,6 +67,10 @@ import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -45,6 +84,14 @@ public class google_login extends AppCompatActivity implements View.OnClickListe
     private ProgressDialog mProgressDialog;
     String serverID = "723836520365-n1bevgandt9o3s35gsnu2het64h50s9a.apps.googleusercontent.com";
 
+    FeedReaderDbHelper mDbHelper = new FeedReaderDbHelper(this);
+
+    IProfile profile;
+
+    AccountHeader headerResult;
+
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -54,7 +101,74 @@ public class google_login extends AppCompatActivity implements View.OnClickListe
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        //mStatusTextView = (TextView) findViewById(R.id.status);
+        mStatusTextView = (TextView) findViewById(R.id.status);
+
+        profile = new ProfileDrawerItem().withName("Profile").withIdentifier(1).withIcon(GoogleMaterial.Icon.gmd_account_circle);
+
+        // Create the AccountHeader
+        headerResult = new AccountHeaderBuilder()
+                .withActivity(this)
+                .withTranslucentStatusBar(true)
+                .withHeaderBackground(R.mipmap.ic_header_background)
+                .addProfiles(
+// Adding profiles to header view
+                        profile,
+                        //don't ask but google uses 14dp for the add account icon in gmail but 20dp for the normal icons (like manage account)
+// Adding setting drawer item other than profile drawer item
+                        new ProfileSettingDrawerItem().withName("Add Account").withIcon(new IconicsDrawable(this, GoogleMaterial.Icon.gmd_add).actionBar().paddingDp(5).colorRes(R.color.material_drawer_primary_text)).withIdentifier(10),
+                        new ProfileSettingDrawerItem().withName("Manage Account").withIcon(GoogleMaterial.Icon.gmd_settings).withIdentifier(11)
+                )
+                .withOnAccountHeaderListener(new AccountHeader.OnAccountHeaderListener() {
+                    @Override
+                    public boolean onProfileChanged(View view, IProfile profile, boolean current) {
+                        //sample usage of the onProfileChanged listener
+                        //if the clicked item has the identifier 1 add a new profile ;)
+                        /*if (profile instanceof IDrawerItem && profile.getIdentifier() == PROFILE_SETTING) {
+                            int count = 100 + headerResult.getProfiles().size() + 1;
+                            IProfile newProfile = new ProfileDrawerItem().withNameShown(true).withName("Batman" + count).withEmail("batman" + count + "@gmail.com").withIcon(R.drawable.profile5).withIdentifier(count);
+                            if (headerResult.getProfiles() != null) {
+                                //we know that there are 2 setting elements. set the new profile above them ;)
+                                headerResult.addProfile(newProfile, headerResult.getProfiles().size() - 2);
+                            } else {
+                                headerResult.addProfiles(newProfile);
+                            }
+                        }*/
+
+                        //false if you have not consumed the event and it should close the drawer
+                        return false;
+                    }
+                })
+                .withSavedInstance(savedInstanceState)
+                .build();
+
+        Drawer result = new DrawerBuilder()
+                .withActivity(this)
+                .withToolbar(toolbar)
+                .withHasStableIds(true)
+                .withItemAnimator(new AlphaCrossFadeAnimator())
+                .withAccountHeader(headerResult) //set the AccountHeader we created earlier for the header
+                .addDrawerItems(
+                        //profile,
+                        //new DividerDrawerItem(),
+                        new PrimaryDrawerItem().withName("Dashboard").withIdentifier(2).withIcon(GoogleMaterial.Icon.gmd_dashboard),
+                        new PrimaryDrawerItem().withName("Exercises").withIdentifier(3).withIcon(GoogleMaterial.Icon.gmd_fitness_center),
+                        new PrimaryDrawerItem().withName("Badges").withIdentifier(4).withIcon(GoogleMaterial.Icon.gmd_local_offer),
+                        new PrimaryDrawerItem().withName("Friends").withIdentifier(5).withIcon(GoogleMaterial.Icon.gmd_people),
+                        new PrimaryDrawerItem().withName("Forums").withIdentifier(6).withIcon(GoogleMaterial.Icon.gmd_forum),
+                        new PrimaryDrawerItem().withName("Settings").withIdentifier(7).withIcon(GoogleMaterial.Icon.gmd_settings),
+                        new PrimaryDrawerItem().withName("Report").withIdentifier(8).withIcon(GoogleMaterial.Icon.gmd_feedback)
+                )
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+                        TextView textView = (TextView) findViewById(R.id.DText);
+                        textView.setText(String.valueOf(drawerItem.getIdentifier()));
+                        selectDrawerItem(drawerItem);
+                        return false;
+                    }
+                })
+                .build();
+
 
         // Button listeners
         findViewById(R.id.sign_in_button).setOnClickListener(this);
@@ -64,7 +178,8 @@ public class google_login extends AppCompatActivity implements View.OnClickListe
         // Configure sign-in to request the user's ID, email address, and basic
         // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestEmail().requestIdToken(serverID).requestProfile()
+                .requestEmail()
+                .requestIdToken(serverID).requestProfile()
                 .build();
 
         // Build a GoogleApiClient with access to the Google Sign-In API and the
@@ -76,6 +191,33 @@ public class google_login extends AppCompatActivity implements View.OnClickListe
 
         SignInButton signInButton = (SignInButton) findViewById(R.id.sign_in_button);
         signInButton.setSize(SignInButton.SIZE_STANDARD);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        //initialize and create the image loader logic
+        DrawerImageLoader.init(new AbstractDrawerImageLoader() {
+            @Override
+            public void set(ImageView imageView, Uri uri, Drawable placeholder) {
+                Picasso.with(imageView.getContext()).load(uri).placeholder(placeholder).into(imageView);
+            }
+
+            @Override
+            public void cancel(ImageView imageView) {
+                Picasso.with(imageView.getContext()).cancelRequest(imageView);
+            }
+
+    /*
+    @Override
+    public Drawable placeholder(Context ctx) {
+        return super.placeholder(ctx);
+    }
+
+    @Override
+    public Drawable placeholder(Context ctx, String tag) {
+        return super.placeholder(ctx, tag);
+    }
+    */
+        });
 
     }
 
@@ -130,12 +272,15 @@ public class google_login extends AppCompatActivity implements View.OnClickListe
         if (result.isSuccess()) {
             // Signed in successfully, show authenticated UI.
             GoogleSignInAccount acct = result.getSignInAccount();
+            firebaseAuthWithGoogle(acct);
             idToken = acct.getIdToken();
             //mStatusTextView.setText(getString(R.string.signed_in_fmt, acct.getDisplayName()));
-            updateUI(true);
+            Log.d(TAG, "Account name:" + acct.getDisplayName());
+            Log.d(TAG, "Account profile image url:" + acct.getPhotoUrl());
+            //DrawerImageLoader.getInstance().setImage((ImageView) profile.generateView(this), result.getSignInAccount().getPhotoUrl(), "Profile");
         } else {
             // Signed out, show unauthenticated UI.
-            updateUI(false);
+            updateUI(null);
         }
         //result.getSignInAccount().getPhotoUrl();
         /*if(idToken != null) {
@@ -143,40 +288,82 @@ public class google_login extends AppCompatActivity implements View.OnClickListe
         }*/
     }
 
-    // [START signOut]
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+        // [START_EXCLUDE silent]
+        //showProgressDialog();
+        // [END_EXCLUDE]
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                            profile.withName(user.getDisplayName());
+                            profile.withEmail(user.getEmail());
+                            if(user.getPhotoUrl() != null) {
+                                profile.withIcon(user.getPhotoUrl().toString());
+                            }
+                            headerResult.updateProfile(profile);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            //Toast.makeText(GoogleSignInActivity.this, "Authentication failed.",
+                                    //Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+
+                        // [START_EXCLUDE]
+                        //hideProgressDialog();
+                        // [END_EXCLUDE]
+                    }
+                });
+    }
+
     private void signOut() {
+        // Firebase sign out
+        mAuth.signOut();
+
+        // Google sign out
         Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(
                 new ResultCallback<Status>() {
                     @Override
-                    public void onResult(Status status) {
-                        // [START_EXCLUDE]
-                        updateUI(false);
-                        // [END_EXCLUDE]
+                    public void onResult(@NonNull Status status) {
+                        updateUI(null);
                     }
                 });
     }
-    // [END signOut]
 
-    // [START revokeAccess]
     private void revokeAccess() {
+        // Firebase sign out
+        mAuth.signOut();
+
+        // Google revoke access
         Auth.GoogleSignInApi.revokeAccess(mGoogleApiClient).setResultCallback(
                 new ResultCallback<Status>() {
                     @Override
-                    public void onResult(Status status) {
-                        // [START_EXCLUDE]
-                        updateUI(false);
-                        // [END_EXCLUDE]
+                    public void onResult(@NonNull Status status) {
+                        updateUI(null);
                     }
                 });
     }
-    // [END revokeAccess]
 
-    private void updateUI(boolean signedIn) {
-        if (signedIn) {
+    private void updateUI(FirebaseUser user) {
+        //hideProgressDialog();
+        if (user != null) {
+            //mStatusTextView.setText(getString(R.string.google_status_fmt, user.getEmail()));
+            //mDetailTextView.setText(getString(R.string.firebase_status_fmt, user.getUid()));
+
             findViewById(R.id.sign_in_button).setVisibility(View.GONE);
             //findViewById(R.id.sign_out_and_disconnect).setVisibility(View.VISIBLE);
         } else {
             //mStatusTextView.setText(R.string.signed_out);
+            //mDetailTextView.setText(null);
 
             findViewById(R.id.sign_in_button).setVisibility(View.VISIBLE);
             //findViewById(R.id.sign_out_and_disconnect).setVisibility(View.GONE);
@@ -188,5 +375,75 @@ public class google_login extends AppCompatActivity implements View.OnClickListe
         // An unresolvable error has occurred and Google APIs (including Sign-In) will not
         // be available.
         Log.d(TAG, "onConnectionFailed:" + connectionResult);
+        Toast.makeText(this, "Google Play Services error.", Toast.LENGTH_SHORT).show();
     }
+
+    public void selectDrawerItem(IDrawerItem menuItem) {
+        Intent intent;
+        // !! Prevent going to any activity if the user is already there, and simply close the drawer
+        switch((int) menuItem.getIdentifier()) {
+            case 1: // !! Add profile activity
+                intent = new Intent(this, google_login.class);
+                startActivity(intent);
+                break;
+            case 2:
+                intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                break;
+            case 3:
+                intent = new Intent(this, Exercises.class);
+                startActivity(intent);
+                break;
+            case 4:
+                intent = new Intent(this, Badges.class);
+                startActivity(intent);
+                break;
+            case 5:
+                intent = new Intent(this, Friends.class);
+                startActivity(intent);
+                break;
+            case 6:
+                intent = new Intent(this, ForumsActivity.class);
+                startActivity(intent);
+                break;
+            case 7:
+                intent = new Intent(this, SettingsActivity.class);
+                startActivity(intent);
+                break;
+            case 8:
+                intent = new Intent(this, ReportActivity.class);
+                startActivity(intent);
+                break;
+            default: // !! Examine more later
+                intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+        }
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null) {
+            updateUI(currentUser);
+            Log.d(TAG, "User email:" + currentUser.getEmail() + "User ID:" + currentUser.getUid() + "Photo url:" + currentUser.getPhotoUrl());
+            profile.withName(currentUser.getDisplayName());
+            profile.withEmail(currentUser.getEmail());
+            if(currentUser.getPhotoUrl() != null) {
+                profile.withIcon(currentUser.getPhotoUrl().toString());
+            }
+            headerResult.updateProfile(profile);
+            //DrawerImageLoader.getInstance().setImage((ImageView) profile.generateView(this), currentUser.getPhotoUrl(), "Profile");
+        }
+        //Toast.makeText(this, "User email:" + currentUser.getEmail() + "User ID:" + currentUser.getUid() + "Photo url:" + currentUser.getPhotoUrl(), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        mDbHelper.close();
+        super.onDestroy();
+    }
+
 }
